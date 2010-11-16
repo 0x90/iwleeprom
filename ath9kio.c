4d7aa59232921fa19ce547c321e91457d45c647b
qpxtool <qpxtool@d71b0ee8-fd71-1909-1bd9-511f2112d154>
2010-11-16 22:57:02 +0000

    iwl cards init and ath9k write support added

--- ath9kio.c
+++ ath9kio.c
@@ -6,7 +6,11 @@
 * ICQ: 152322, Skype: ittr1um		
 * Copyright (C) 2010, Gennady "ShultZ" Kozlov <qpxtool@mail.ru>
 *
-* 
+*
+* some values and HW identify code got from Atheros ath9k Linux driver
+* Copyright (c) 2008-2010 Atheros Communications Inc.
+*
+*
 * This program is free software; you can redistribute it and/or modify
 * it under the terms of the GNU General Public License as published by
 * the Free Software Foundation; either version 2 of the License, or
@@ -22,15 +26,58 @@
 
 #include "ath9kio.h"
 
-#define ATH_EEPROM_SIGNATURE    0x7801
+#define ATH_EEPROM_SIGNATURE        0x7801
 #define ATH_MMAP_LENGTH 0x10000
 
-/* ATH9K defines */
+
 #define AR5416_EEPROM_S             2
 #define AR5416_EEPROM_OFFSET        0x2000
 
-//#define AR_EEPROM_STATUS_DATA                    (AR_SREV_9300_20_OR_LATER(ah) ? 0x4084 : 0x407c)
-#define AR_EEPROM_STATUS_DATA                    ((dev->dev > 0x0030) ? 0x4084 : 0x407c)
+
+#define AR_SREV_VERSION_5416_PCI    0x00D
+#define AR_SREV_VERSION_5416_PCIE   0x00C
+#define AR_SREV_VERSION_9100        0x014
+#define AR_SREV_VERSION_9160        0x040
+#define AR_SREV_VERSION_9280        0x080
+#define AR_SREV_VERSION_9285        0x0C0
+#define AR_SREV_VERSION_9287        0x180
+#define AR_SREV_VERSION_9271        0x140
+#define AR_SREV_VERSION_9300        0x1c0
+#define AR_SREV_REVISION_9300_20    2 /* 2.0 and 2.1 */
+
+#define AR_SREV_9100 \
+	(macVer == AR_SREV_VERSION_9100)
+#define AR_SREV_9300_20_OR_LATER \
+	((macVer > AR_SREV_VERSION_9300) || \
+	 ((macVer == AR_SREV_VERSION_9300) && \
+	  (macRev >= AR_SREV_REVISION_9300_20)))
+
+#define AR_SREV                     ((AR_SREV_9100) ? 0x0600 : 0x4020)
+#define AR_SREV_ID                  ((AR_SREV_9100) ? 0x00000FFF : 0x000000FF)
+#define AR_SREV_VERSION             0x000000F0
+#define AR_SREV_VERSION_S           4
+#define AR_SREV_REVISION            0x00000007
+#define AR_SREV_VERSION2            0xFFFC0000
+#define AR_SREV_VERSION2_S          18
+#define AR_SREV_TYPE2               0x0003F000
+#define AR_SREV_TYPE2_S             12
+#define AR_SREV_TYPE2_CHAIN         0x00001000
+#define AR_SREV_TYPE2_HOST_MODE     0x00002000
+#define AR_SREV_REVISION2           0x00000F00
+#define AR_SREV_REVISION2_S         8
+
+
+#define AR_GPIO_IN_OUT              0x4048
+#define AR_GPIO_OE_OUT              (AR_SREV_9300_20_OR_LATER ? 0x4050 : 0x404c)
+#define AR_GPIO_OE_OUT_DRV          0x3
+#define AR_GPIO_OE_OUT_DRV_NO       0x0
+#define AR_GPIO_OE_OUT_DRV_LOW      0x1
+#define AR_GPIO_OE_OUT_DRV_HI       0x2
+#define AR_GPIO_OE_OUT_DRV_ALL      0x3
+
+#define AR_GPIO_OUTPUT_MUX1                      (AR_SREV_9300_20_OR_LATER ? 0x4068 : 0x4060)
+
+#define AR_EEPROM_STATUS_DATA                    (AR_SREV_9300_20_OR_LATER ? 0x4084 : 0x407c)
 #define AR_EEPROM_STATUS_DATA_VAL                0x0000ffff
 #define AR_EEPROM_STATUS_DATA_VAL_S              0
 #define AR_EEPROM_STATUS_DATA_BUSY               0x00010000
@@ -38,23 +85,97 @@
 #define AR_EEPROM_STATUS_DATA_PROT_ACCESS        0x00040000
 #define AR_EEPROM_STATUS_DATA_ABSENT_ACCESS      0x00080000
 
+static struct {
+	uint32_t version;
+	const char * name;
+} ath_mac_bb_names[] = {
+	/* Devices with external radios */
+	{ AR_SREV_VERSION_5416_PCI,  "5416" },
+	{ AR_SREV_VERSION_5416_PCIE, "5418" },
+	{ AR_SREV_VERSION_9100,      "9100" },
+	{ AR_SREV_VERSION_9160,      "9160" },
+	/* Single-chip solutions */
+	{ AR_SREV_VERSION_9280,      "9280" },
+	{ AR_SREV_VERSION_9285,      "9285" },
+	{ AR_SREV_VERSION_9287,      "9287" },
+	{ AR_SREV_VERSION_9271,      "9271" },
+	{ AR_SREV_VERSION_9300,      "9300" },
+	{ 0, ""}
+};
+
+static const char *ath9k_hw_name(uint32_t mac_bb_version)
+{
+	int i;
+	for (i=0; ath_mac_bb_names[i].version; i++)
+		if (ath_mac_bb_names[i].version == mac_bb_version)
+			return ath_mac_bb_names[i].name;
+	return "????";
+}
+
 #define WAIT_TIMEOUT 10000 /* x10 us */
 
-/* PCI R/W macros */
-#define PCI_IN32(dev,a)    (*((volatile unsigned long int *)(dev->mem + (a))))
-#define PCI_OUT32(dev,v,a) (*((volatile unsigned long int *)(dev->mem + (a))) = (v))
+uint32_t eeprom_base,
+		 macVer,
+		 macRev;
+bool	 isPCIE;
 
-void ath9k_eeprom_lock(struct pcidev *dev) {}
+static const uint16_t ath9k_eeprom_read16_raw(struct pcidev *dev, unsigned int addr);
+static const uint16_t ath9k_eeprom_read16(struct pcidev *dev, unsigned int addr);
+static void ath9k_eeprom_write16(struct pcidev *dev, unsigned int addr, uint16_t value);
 
-void ath9k_eeprom_unlock(struct pcidev *dev) {}
 
-const uint16_t ath9k_eeprom_read16(struct pcidev *dev, unsigned int addr)
+static void ath9k_read_hw_version(struct pcidev *dev)
+{
+	uint32_t val;
+
+	memcpy(&val, dev->mem + AR_SREV, 4);
+	val &= AR_SREV_ID;
+
+	if (val == 0xFF) {
+		memcpy(&val, dev->mem + AR_SREV, 4);
+		macVer = (val & AR_SREV_VERSION2) >> AR_SREV_TYPE2_S;
+		macRev = (val & AR_SREV_REVISION2) >> AR_SREV_REVISION2_S;
+		isPCIE = (val & AR_SREV_TYPE2_HOST_MODE) ? 0 : 1;
+	} else {
+		if (!AR_SREV_9100)
+			macVer = (val & AR_SREV_VERSION) >> AR_SREV_VERSION_S;
+		macRev = val & AR_SREV_REVISION;
+		if (macVer == AR_SREV_VERSION_5416_PCIE)
+			isPCIE = 1;
+	}
+}
+
+static bool ath9k_eeprom_lock(struct pcidev *dev) {
+// reading HW version, some register addresses depends on it
+	ath9k_read_hw_version(dev);
+	printf("HW ver AR%s (PCI%s) rev %04x", ath9k_hw_name(macVer), isPCIE ? "-E" : "", macRev);
+
+// reading EEPROM size and setting it's base address
+// thanks to Inv from forum.ixbt.com
+	dev->ops->eeprom_size = 0;
+	if (376  == ath9k_eeprom_read16_raw(dev,  64))      { dev->ops->eeprom_size =  376; eeprom_base =  64; dev->ops->eeprom_signature = 0x0178; }
+	else if (3256 == ath9k_eeprom_read16_raw(dev, 256)) { dev->ops->eeprom_size = 3256; eeprom_base = 256; dev->ops->eeprom_signature = 0x0cb8; }
+	else if (727  == ath9k_eeprom_read16_raw(dev, 128)) { dev->ops->eeprom_size =  727; eeprom_base = 128; dev->ops->eeprom_signature = 0x02d7; }
+	if (!dev->ops->eeprom_size) {
+		printf("Can't get ath9k eeprom size!\n");
+		return false;
+	} else {
+		printf("ath9k eeprom size: %d\n", dev->ops->eeprom_size);
+		return true;
+	}
+}
+
+static bool ath9k_eeprom_release(struct pcidev *dev) { return true; }
+
+static const uint16_t ath9k_eeprom_read16_raw(struct pcidev *dev, unsigned int addr)
 {
 	int32_t data;
 	int i;
+// requesting data
 	memcpy(&data, dev->mem + AR5416_EEPROM_OFFSET + (addr << AR5416_EEPROM_S), 4);
 
-	for(i = 0; i < WAIT_TIMEOUT; i++) {
+// waiting...
+	for(i = WAIT_TIMEOUT; i; i--) {
 		usleep(10);
 		memcpy(&data, dev->mem + AR_EEPROM_STATUS_DATA, 4);
 		if ( 0 == (data & (AR_EEPROM_STATUS_DATA_BUSY | AR_EEPROM_STATUS_DATA_PROT_ACCESS))) {
@@ -66,25 +187,78 @@ const uint16_t ath9k_eeprom_read16(struct pcidev *dev, unsigned int addr)
 	return 0;
 }
 
-void ath9k_eeprom_write16(struct pcidev *dev, unsigned int addr, uint16_t value)
+static const uint16_t ath9k_eeprom_read16(struct pcidev *dev, unsigned int addr)
 {
-
-
+	return ath9k_eeprom_read16_raw(dev, addr + eeprom_base);
 }
 
-void ath9k_eeprom_patch11n(struct pcidev *dev)
+static void ath9k_eeprom_write16(struct pcidev *dev, unsigned int addr, uint16_t value)
+{
+	int i;
+	uint32_t data;
+	uint32_t
+			gpio_out_mux1,
+			gpio_oe_out,
+			gpio_in_out;
+
+// selecting GPIO
+	memcpy(&gpio_out_mux1, dev->mem + AR_GPIO_OUTPUT_MUX1, 4);
+	data = (gpio_out_mux1 & 0xFFF07FFF);
+	memcpy(dev->mem + AR_GPIO_OUTPUT_MUX1, &data, 4);
+	usleep(10);
+
+// setting GPIO pin direction
+	memcpy(&gpio_oe_out, dev->mem + AR_GPIO_OE_OUT, 4);
+	data = (gpio_oe_out & 0xFFFFFF3F) | 0x000000C0;
+	memcpy(dev->mem + AR_GPIO_OE_OUT, &data, 4);
+	usleep(10);
+
+// setting GPIO pin level
+	memcpy(&gpio_in_out, dev->mem + AR_GPIO_IN_OUT, 4);
+	data = (gpio_in_out & 0xFFFFFFF7);
+	memcpy(dev->mem + AR_GPIO_IN_OUT, &data, 4);
+	usleep(10);
+
+// sending data
+	memcpy(dev->mem + AR5416_EEPROM_OFFSET + (addr << AR5416_EEPROM_S), &data, 4);
+
+// waiting...
+	for(i = WAIT_TIMEOUT; i; i--) {
+		usleep(10);
+		memcpy(&data, dev->mem + AR_EEPROM_STATUS_DATA, 4);
+		if ( 0 == (data & ( AR_EEPROM_STATUS_DATA_BUSY | 
+							AR_EEPROM_STATUS_DATA_BUSY_ACCESS | 
+							AR_EEPROM_STATUS_DATA_PROT_ACCESS | 
+							AR_EEPROM_STATUS_DATA_ABSENT_ACCESS)) )
+			break;
+	}
+
+// restoring GPIO state...
+	memcpy(dev->mem + AR_GPIO_IN_OUT, &gpio_in_out, 4);
+	memcpy(dev->mem + AR_GPIO_OE_OUT, &gpio_oe_out, 4);
+	memcpy(dev->mem + AR_GPIO_OUTPUT_MUX1, &gpio_out_mux1, 4);
+
+	if (!i)
+		printf("timeout writing ath9k eeprom at %04x!\n", addr);
+}
+
+static void ath9k_eeprom_patch11n(struct pcidev *dev)
 {
 	printf("ath9k 802.11n patch not implemented yet.\n");
 }
 
 struct dev_ops dev_ops_ath9k = {
+	.name			 = "ath9k",
 	.mmap_size        = ATH_MMAP_LENGTH,
-	.eeprom_signature = ATH_EEPROM_SIGNATURE,
+	.eeprom_size	  = 0,
+	.eeprom_signature = 0,
+	.eeprom_writable  = true,
 
+	.init_device	 = NULL,
 	.eeprom_lock     = &ath9k_eeprom_lock,
-	.eeprom_unlock   = &ath9k_eeprom_unlock,
+	.eeprom_release  = &ath9k_eeprom_release,
 	.eeprom_read16   = &ath9k_eeprom_read16,
 	.eeprom_write16  = &ath9k_eeprom_write16,
-	.eeprom_patch11n = &ath9k_eeprom_patch11n
+	.eeprom_patch11n = &ath9k_eeprom_patch11n,
+	.eeprom_parse    = NULL
 };
-
--- iwleeprom.c
+++ iwleeprom.c
@@ -51,6 +51,8 @@ static struct option long_options[] = {
 	{"help",      0, NULL, 'h'},
 	{"list",      0, NULL, 'l'},
 	{"debug",     1, NULL, 'D'},
+	{"parse",     0, NULL, 'P'},
+	{"init",      0, NULL, 'I'},
 	{"patch11n",  0, NULL, 'p'}
 };
 
@@ -63,6 +65,8 @@ uid_t ruid,euid,suid;
 char	*ifname = NULL,
 		*ofname = NULL;
 bool patch11n = false,
+	 init_device = false,
+	 parse = false,
 	 nodev = false,
 	 preserve_mac = false,
 	 preserve_calib = false;
@@ -75,45 +79,58 @@ struct pcidev dev;
 
 #define DEVICES_PATH "/sys/bus/pci/devices"
 
-struct pci_id valid_ids[] = {
+const struct pci_id valid_ids[] = {
 /* Intel devices */
-	{ INTEL_PCI_VID,   0x0082, 0, IWL_EEPROM_SIZE_5K,	&dev_ops_iwl, "6000 Series Gen2"},
-	{ INTEL_PCI_VID,   0x0083, 0, IWL_EEPROM_SIZE_5K,	&dev_ops_iwl, "Centrino Wireless-N 1000"},
-	{ INTEL_PCI_VID,   0x0084, 0, IWL_EEPROM_SIZE_5K,	&dev_ops_iwl, "Centrino Wireless-N 1000"},
-	{ INTEL_PCI_VID,   0x0085, 0, IWL_EEPROM_SIZE_5K,	&dev_ops_iwl, "6000 Series Gen2"},
-	{ INTEL_PCI_VID,   0x0087, 0, IWL_EEPROM_SIZE_5K,	&dev_ops_iwl, "Centrino Advanced-N + WiMAX 6250"},
-	{ INTEL_PCI_VID,   0x0089, 0, IWL_EEPROM_SIZE_5K,	&dev_ops_iwl, "Centrino Advanced-N + WiMAX 6250"},
-	{ INTEL_PCI_VID,   0x0885, 0, IWL_EEPROM_SIZE_5K,	&dev_ops_iwl, "WiFi+WiMAX 6050 Series Gen2"},
-	{ INTEL_PCI_VID,   0x0886, 0, IWL_EEPROM_SIZE_5K,	&dev_ops_iwl, "WiFi+WiMAX 6050 Series Gen2"},
-	{ INTEL_PCI_VID,   0x4229, 1, IWL_EEPROM_SIZE_4965,	&dev_ops_iwl, "PRO/Wireless 4965 AG or AGN [Kedron] Network Connection"},
-	{ INTEL_PCI_VID,   0x422b, 0, IWL_EEPROM_SIZE_5K,	&dev_ops_iwl, "Centrino Ultimate-N 6300"},
-	{ INTEL_PCI_VID,   0x422c, 0, IWL_EEPROM_SIZE_5K,	&dev_ops_iwl, "Centrino Advanced-N 6200"},
-	{ INTEL_PCI_VID,   0x4230, 1, IWL_EEPROM_SIZE_4965,	&dev_ops_iwl, "PRO/Wireless 4965 AG or AGN [Kedron] Network Connection"},
-	{ INTEL_PCI_VID,   0x4232, 1, IWL_EEPROM_SIZE_5K,	&dev_ops_iwl, "WiFi Link 5100"},
-	{ INTEL_PCI_VID,   0x4235, 1, IWL_EEPROM_SIZE_5K,	&dev_ops_iwl, "Ultimate N WiFi Link 5300"},
-	{ INTEL_PCI_VID,   0x4236, 1, IWL_EEPROM_SIZE_5K,	&dev_ops_iwl, "Ultimate N WiFi Link 5300"},
-	{ INTEL_PCI_VID,   0x4237, 1, IWL_EEPROM_SIZE_5K,	&dev_ops_iwl, "PRO/Wireless 5100 AGN [Shiloh] Network Connection"},
-	{ INTEL_PCI_VID,   0x4238, 0, IWL_EEPROM_SIZE_5K,	&dev_ops_iwl, "Centrino Ultimate-N 6300"},
-	{ INTEL_PCI_VID,   0x4239, 0, IWL_EEPROM_SIZE_5K,	&dev_ops_iwl, "Centrino Advanced-N 6200"},
-	{ INTEL_PCI_VID,   0x423a, 1, IWL_EEPROM_SIZE_5K,	&dev_ops_iwl, "PRO/Wireless 5350 AGN [Echo Peak] Network Connection"},
-	{ INTEL_PCI_VID,   0x423b, 1, IWL_EEPROM_SIZE_5K,	&dev_ops_iwl, "PRO/Wireless 5350 AGN [Echo Peak] Network Connection"},
-	{ INTEL_PCI_VID,   0x423c, 1, IWL_EEPROM_SIZE_5K,	&dev_ops_iwl, "WiMAX/WiFi Link 5150"},
-	{ INTEL_PCI_VID,   0x423d, 1, IWL_EEPROM_SIZE_5K,	&dev_ops_iwl, "WiMAX/WiFi Link 5150"},
+	{ INTEL_PCI_VID,   0x0082, &dev_ops_iwl6k, "6000 Series Gen2"},
+	{ INTEL_PCI_VID,   0x0083, &dev_ops_iwl6k, "Centrino Wireless-N 1000"},
+	{ INTEL_PCI_VID,   0x0084, &dev_ops_iwl6k, "Centrino Wireless-N 1000"},
+	{ INTEL_PCI_VID,   0x0085, &dev_ops_iwl6k, "6000 Series Gen2"},
+	{ INTEL_PCI_VID,   0x0087, &dev_ops_iwl6k, "Centrino Advanced-N + WiMAX 6250"},
+	{ INTEL_PCI_VID,   0x0089, &dev_ops_iwl6k, "Centrino Advanced-N + WiMAX 6250"},
+	{ INTEL_PCI_VID,   0x0885, &dev_ops_iwl6k, "WiFi+WiMAX 6050 Series Gen2"},
+	{ INTEL_PCI_VID,   0x0886, &dev_ops_iwl6k, "WiFi+WiMAX 6050 Series Gen2"},
+	{ INTEL_PCI_VID,   0x4229, &dev_ops_iwl4965, "PRO/Wireless 4965 AG or AGN [Kedron] Network Connection"},
+	{ INTEL_PCI_VID,   0x422b, &dev_ops_iwl6k, "Centrino Ultimate-N 6300"},
+	{ INTEL_PCI_VID,   0x422c, &dev_ops_iwl6k, "Centrino Advanced-N 6200"},
+	{ INTEL_PCI_VID,   0x4230, &dev_ops_iwl4965, "PRO/Wireless 4965 AG or AGN [Kedron] Network Connection"},
+	{ INTEL_PCI_VID,   0x4232, &dev_ops_iwl5k, "WiFi Link 5100"},
+	{ INTEL_PCI_VID,   0x4235, &dev_ops_iwl5k, "Ultimate N WiFi Link 5300"},
+	{ INTEL_PCI_VID,   0x4236, &dev_ops_iwl5k, "Ultimate N WiFi Link 5300"},
+	{ INTEL_PCI_VID,   0x4237, &dev_ops_iwl5k, "PRO/Wireless 5100 AGN [Shiloh] Network Connection"},
+	{ INTEL_PCI_VID,   0x4238, &dev_ops_iwl6k, "Centrino Ultimate-N 6300"},
+	{ INTEL_PCI_VID,   0x4239, &dev_ops_iwl6k, "Centrino Advanced-N 6200"},
+	{ INTEL_PCI_VID,   0x423a, &dev_ops_iwl5k, "PRO/Wireless 5350 AGN [Echo Peak] Network Connection"},
+	{ INTEL_PCI_VID,   0x423b, &dev_ops_iwl5k, "PRO/Wireless 5350 AGN [Echo Peak] Network Connection"},
+	{ INTEL_PCI_VID,   0x423c, &dev_ops_iwl5k, "WiMAX/WiFi Link 5150"},
+	{ INTEL_PCI_VID,   0x423d, &dev_ops_iwl5k, "WiMAX/WiFi Link 5150"},
 
-/* Atheros devices */
-	{ ATHEROS_PCI_VID, 0x0023, 0, ATH_EEPROM_SIZE, &dev_ops_ath9k, "AR5008 Wireless Adapter (PCI)" },
-	{ ATHEROS_PCI_VID, 0x0024, 0, ATH_EEPROM_SIZE, &dev_ops_ath9k, "AR5008 Wireless Adapter (PCI-E)" },
-	{ ATHEROS_PCI_VID, 0x0027, 0, ATH_EEPROM_SIZE, &dev_ops_ath9k, "AR9160 802.11abgn Wireless Adapter (PCI)" },
-	{ ATHEROS_PCI_VID, 0x0029, 0, ATH_EEPROM_SIZE, &dev_ops_ath9k, "AR922X Wireless Adapter (PCI)" },
-	{ ATHEROS_PCI_VID, 0x002A, 0, ATH_EEPROM_SIZE, &dev_ops_ath9k, "AR928X Wireless Adapter (PCI-E)" },
-	{ ATHEROS_PCI_VID, 0x002B, 0, ATH_EEPROM_SIZE, &dev_ops_ath9k, "AR9285 Wireless Adapter (PCI-E)" },
-	{ ATHEROS_PCI_VID, 0x002C, 0, ATH_EEPROM_SIZE, &dev_ops_ath9k, "AR2427 Wireless Adapter (PCI-E)" }, /* PCI-E 802.11n bonded out */
-	{ ATHEROS_PCI_VID, 0x002D, 0, ATH_EEPROM_SIZE, &dev_ops_ath9k, "AR9287 Wireless Adapter (PCI)" }, /* PCI   */
-	{ ATHEROS_PCI_VID, 0x002E, 0, ATH_EEPROM_SIZE, &dev_ops_ath9k, "AR9287 Wireless Adapter (PCI-E)" }, /* PCI-E */
-	{ ATHEROS_PCI_VID, 0x0030, 0, ATH_EEPROM_SIZE, &dev_ops_ath9k, "AR9300 Wireless Adapter (PCI-E)" }, /* PCI-E  AR9300 */
-//	{ ATHEROS_PCI_VID, 0x0033, 0, ATH_EEPROM_SIZE, &dev_ops_ath9k, "11a/b/g/n Wireless LAN Mini-PCI Express Adapter" },
+/* Atheros 5k devices */
+	{ ATHEROS_PCI_VID, 0x0007, &dev_ops_ath5k, "AR5000 802.11a Wireless Adapter" },
+	{ ATHEROS_PCI_VID, 0x0011, &dev_ops_ath5k, "AR5210 802.11a NIC" },
+	{ ATHEROS_PCI_VID, 0x0012, &dev_ops_ath5k, "AR5211 802.11ab NIC" },
+	{ ATHEROS_PCI_VID, 0x0013, &dev_ops_ath5k, "Atheros AR5001X+ Wireless Network Adapter" },
+	{ ATHEROS_PCI_VID, 0x001a, &dev_ops_ath5k, "AR2413 802.11bg NIC" },
+	{ ATHEROS_PCI_VID, 0x001b, &dev_ops_ath5k, "AR5413 802.11abg NIC" },
+	{ ATHEROS_PCI_VID, 0x001c, &dev_ops_ath5k, "AR5001 Wireless Network Adapter" },
+	{ ATHEROS_PCI_VID, 0x001d, &dev_ops_ath5k, "AR5007G Wireless Network Adapter" },
+	{ ATHEROS_PCI_VID, 0x0020, &dev_ops_ath5k, "AR5513 802.11abg Wireless NIC" },
+	{ ATHEROS_PCI_VID, 0x0207, &dev_ops_ath5k, "AR5210 802.11abg" },
+	{ ATHEROS_PCI_VID, 0x1014, &dev_ops_ath5k, "AR5212 802.11abg" },
 
-	{ 0, 0, 0, 0, NULL, "" }
+/* Atheros 9k devices */
+	{ ATHEROS_PCI_VID, 0x0023, &dev_ops_ath9k, "AR5008 Wireless Adapter (PCI)" },
+	{ ATHEROS_PCI_VID, 0x0024, &dev_ops_ath9k, "AR5008 Wireless Adapter (PCI-E)" },
+	{ ATHEROS_PCI_VID, 0x0027, &dev_ops_ath9k, "AR9160 802.11abgn Wireless Adapter (PCI)" },
+	{ ATHEROS_PCI_VID, 0x0029, &dev_ops_ath9k, "AR922X Wireless Adapter (PCI)" },
+	{ ATHEROS_PCI_VID, 0x002A, &dev_ops_ath9k, "AR928X Wireless Adapter (PCI-E)" },
+	{ ATHEROS_PCI_VID, 0x002B, &dev_ops_ath9k, "AR9285 Wireless Adapter (PCI-E)" },
+	{ ATHEROS_PCI_VID, 0x002C, &dev_ops_ath9k, "AR2427 Wireless Adapter (PCI-E)" }, /* PCI-E 802.11n bonded out */
+	{ ATHEROS_PCI_VID, 0x002D, &dev_ops_ath9k, "AR9287 Wireless Adapter (PCI)" },
+	{ ATHEROS_PCI_VID, 0x002E, &dev_ops_ath9k, "AR9287 Wireless Adapter (PCI-E)" },
+	{ ATHEROS_PCI_VID, 0x0030, &dev_ops_ath9k, "AR9300 Wireless Adapter (PCI-E)" },
+//	{ ATHEROS_PCI_VID, 0x0033, &dev_ops_ath9k, "11a/b/g/n Wireless LAN Mini-PCI Express Adapter" },
+
+	{ 0, 0, NULL, "" }
 };
 
 void init_card()
@@ -139,17 +156,23 @@ void release_card()
 void init_dump(struct pcidev *dev, char *filename)
 {
 	FILE *fd;
+	uint32_t eeprom_size;
 
 	seteuid(ruid);
 	if (!(fd = fopen(filename, "rb")))
-		die("Can't read file %s\n", filename);
-	dev->eeprom_size = 2 * fread(buf, 2, EEPROM_SIZE_MAX/2, fd);
+		die("Can't read file '%s'\n", filename);
+	eeprom_size = 2 * fread(buf, 2, EEPROM_SIZE_MAX/2, fd);
 	fclose(fd);
 	seteuid(suid);
 
-	printf("Dump file: %s (read %Ld bytes)\n", filename, (uint64_t) dev->eeprom_size);
-	if(dev->eeprom_size < IWL_EEPROM_SIZE_4965)
+	printf("Dump file: '%s' (read %u bytes)\n", filename, eeprom_size);
+	if(eeprom_size <dev_ops_iwl4965.eeprom_size)
 		die("Too small file!\n");
+
+	if(eeprom_size == dev_ops_iwl4965.eeprom_size)
+		dev->ops = &dev_ops_iwl4965;	
+	else
+		dev->ops = &dev_ops_iwl5k;
 
 	if ( dev->ops->eeprom_signature == le2cpu16(buf[0])) {
 		dump_order = order_le;
@@ -167,9 +190,9 @@ void fixate_dump(struct pcidev *dev, char *filename)
 	seteuid(ruid);
 
 	if (!(fd = fopen(filename, "wb")))
-		die("Can't create file %s\n", filename);
-	fwrite(buf, dev->eeprom_size, 1, fd);
-	printf("Dump file written: %s\n", filename);
+		die("Can't create file '%s'\n", filename);
+	fwrite(buf, dev->ops->eeprom_size, 1, fd);
+	printf("Dump file written: '%s'\n", filename);
 	fclose(fd);
 
 	seteuid(suid);
@@ -198,37 +221,34 @@ void eeprom_read(char *filename)
 {
 	unsigned int addr = 0;
 	FILE *fd;
-	dev.eeprom_size = valid_ids[dev.idx].eeprom_size;
 	
-	seteuid(ruid);
-	if (!(fd = fopen(filename, "wb")))
-		die("Can't create file %s\n", filename);
-	seteuid(suid);
-
-	dev.ops->eeprom_lock(&dev);
+	if (!dev.ops->eeprom_lock(&dev))
+		die("eeprom_lock() failed!\n");
 
 	printf("Saving dump with byte order: %s ENDIAN\n", (dump_order == order_le) ? "LITTLE":"BIG");
 
-	for (addr = 0; addr < dev.eeprom_size; addr += 2)
+	for (addr = 0; addr < dev.ops->eeprom_size; addr += 2)
 	{
 		if (dump_order == order_le)
 			buf[addr/2] = cpu2le16( dev.ops->eeprom_read16(&dev, addr) );
 		else
 			buf[addr/2] = cpu2be16( dev.ops->eeprom_read16(&dev, addr) );
 		if (0 ==(addr & 0x7F)) printf("%04x [", addr);
-		printf(".");
+		printf("x");
 		if (0x7E ==(addr & 0x7F)) printf("]\n");
 		fflush(stdout);
 	}
 
 	seteuid(ruid);
-	fwrite(buf, dev.eeprom_size, 1, fd);
+	if (!(fd = fopen(filename, "wb")))
+		die("Can't create file '%s'\n", filename);
+	fwrite(buf, dev.ops->eeprom_size, 1, fd);
 	fclose(fd);
 	seteuid(suid);
 
-	dev.ops->eeprom_unlock(&dev);
+	dev.ops->eeprom_release(&dev);
 
-	printf("\nEEPROM has been dumped to %s\n", filename);
+	printf("\nEEPROM has been dumped to '%s'\n", filename);
 }
 
 void eeprom_write(char *filename)
@@ -238,23 +258,24 @@ void eeprom_write(char *filename)
 	uint16_t value;
 	size_t   size;
 	FILE *fd;
-	dev.eeprom_size = valid_ids[dev.idx].eeprom_size;
 
 	seteuid(ruid);
 	if (!(fd = fopen(filename, "rb")))
-		die("Can't read file %s\n", filename);
-	size = 2 * fread(buf, 2, dev.eeprom_size/2, fd);
+		die("Can't read file '%s'\n", filename);
+	size = 2 * fread(buf, 2, dev.ops->eeprom_size/2, fd);
 	fclose(fd);
 	seteuid(suid);
 
-	dev.ops->eeprom_lock(&dev);
+	if (!dev.ops->eeprom_lock(&dev))
+		die("eeprom_lock() failed!\n");
 
+	printf("Writing data to EEPROM...\n  '.' = match, 'x' = write\n");
 	for(addr=0; addr<size;addr+=2)
 	{
 		if (order == order_unknown) {
-			if ( dev.ops->eeprom_signature == le2cpu16(buf[addr/2])) {
+			if ( buf[addr/2] && dev.ops->eeprom_signature == le2cpu16(buf[addr/2]) ) {
 				order = order_le;
-			} else if ( dev.ops->eeprom_signature == be2cpu16(buf[addr/2])) {
+			} else if ( buf[addr/2] && dev.ops->eeprom_signature == be2cpu16(buf[addr/2]) ) {
 				order = order_be;
 			} else {
 				die("Invalid EEPROM signature!\n");
@@ -269,19 +290,18 @@ void eeprom_write(char *filename)
 		if (0 ==(addr & 0x7F)) printf("%04x [", addr);
 		if (dev.ops->eeprom_read16(&dev, addr) != value) {
 			dev.ops->eeprom_write16(&dev, addr, value);
-			printf(".");
+			printf("x");
 		} else {
-			printf("=");
+			printf(".");
 		}
 		if (0x7E ==(addr & 0x7F)) printf("]\n");
 		fflush(stdout);
 	}
 
-	dev.ops->eeprom_unlock(&dev);
+	dev.ops->eeprom_release(&dev);
 
-	printf("\nEEPROM has been written from %s\n", filename);
+	printf("\nEEPROM has been written from '%s'\n", filename);
 }
-
 
 void die(  const char* format, ... ) {
 	va_list args;
@@ -334,10 +354,11 @@ void list_supported()
 	int i;
 	printf("Known devices:\n");
 	for(i=0; valid_ids[i].ven; i++)
-		printf("  %04x:%04x [%s] %s \n", 
+		printf("  %04x:%04x [%s, %s] %s \n", 
 			valid_ids[i].ven,
 			valid_ids[i].dev,
-			valid_ids[i].writable ? "RW" : "RO",
+			valid_ids[i].ops->eeprom_writable ? "RW" : "RO",
+			valid_ids[i].ops->name,
 			valid_ids[i].name);
 }
 
@@ -392,8 +413,9 @@ void search_card()
 			if (id.idx < 0)
 				printf("\n");
 			else
-				printf(" [%s] %s \n", 
-					valid_ids[id.idx].writable ? "RW" : "RO",
+				printf(" [%s, %s] %s \n", 
+					valid_ids[id.idx].ops->eeprom_writable ? "RW" : "RO",
+					valid_ids[id.idx].ops->name,
 					valid_ids[id.idx].name);
 		}
 		if (id.idx >=0 ) {
@@ -417,7 +439,7 @@ void search_card()
 	for (i=0; i<cnt; i++) {
 		printf("  [%d] %s [%s] %s (%04x:%04x, %04x:%04x)\n", i+1,
 			ids[i].device,
-			valid_ids[ids[i].idx].writable ? "RW" : "RO",
+			valid_ids[ids[i].idx].ops->eeprom_writable ? "RW" : "RO",
 			valid_ids[ids[i].idx].name,
 			ids[i].ven,  ids[i].dev,
 			ids[i].sven, ids[i].sdev);
@@ -451,7 +473,7 @@ int main(int argc, char** argv)
 	getresuid(&ruid, &euid, &suid);
 
 	while (1) {
-		c = getopt_long(argc, argv, "rwld:mcni:o:bhpD:", long_options, NULL);
+		c = getopt_long(argc, argv, "rwld:mcni:o:bhpPID:", long_options, NULL);
 		if (c == -1)
 			break;
 		switch(c) {
@@ -485,8 +507,14 @@ int main(int argc, char** argv)
 			case 'b':
 				dump_order = order_be;
 				break;
+			case 'P':
+				parse = true;
+				break;
 			case 'p':
 				patch11n = true;
+				break;
+			case 'I':
+				init_device = true;
 				break;
 			case 'D':
 				debug = atoi(optarg);
@@ -515,6 +543,10 @@ int main(int argc, char** argv)
 					"save dump in big-endian byteorder (default: little-endian)\n"
 					"\t-p | --patch11n\t\t\t\t"
 					"patch device eeprom to enable 802.11n\n"
+					"\t-I | --init\t\t\t\t"
+					"init device (useful if driver didn't it)\n"
+					"\t-P | --parse\t\t\t\t"
+					"parse eeprom (show available modes/channels)"
 					"\t-l | --list\t\t\t\t"
 					"list known cards\n"
 					"\t-D <level> | --debug <level>\t\t"
@@ -538,8 +570,10 @@ int main(int argc, char** argv)
 
 	printf("Using device %s [%s] %s \n",
 		dev.device,
-		valid_ids[dev.idx].writable ? "RW" : "RO",
+		valid_ids[dev.idx].ops->eeprom_writable ? "RW" : "RO",
 		valid_ids[dev.idx].name);
+	printf("IO driver: %s\n",
+		dev.ops->name);
 
 	map_device();
 
@@ -549,19 +583,30 @@ int main(int argc, char** argv)
 	if (debug)
 		printf("address: %08x\n", offset);
 
-	if(!ifname && !ofname && !patch11n)
-		printf("No file names given or patch option selected!\nNo EEPROM actions will be performed, just write-enable test\n");
+	if(!ifname && !ofname && !patch11n && !parse)
+		printf("No file names given nor actions selected!\nNo EEPROM actions will be performed, just write-enable test\n");
 
 	init_card();
+
+	if (init_device && dev.ops->init_device)
+		dev.ops->init_device(&dev);
 
 	if (ofname)
 		eeprom_read(ofname);
 
-	if (ifname && valid_ids[dev.idx].writable)
+	if (parse && dev.ops->eeprom_parse)
+		dev.ops->eeprom_parse(&dev);
+
+	if (ifname && valid_ids[dev.idx].ops->eeprom_writable)
 		eeprom_write(ifname);
 
-	if (patch11n && valid_ids[dev.idx].writable)
+	if (patch11n && valid_ids[dev.idx].ops->eeprom_writable)
 		dev.ops->eeprom_patch11n(&dev);
+
+	if (parse && dev.ops->eeprom_parse && (ifname || patch11n)) {
+		printf("\n\ndevice capabilities after eeprom writing:\n");
+		dev.ops->eeprom_parse(&dev);
+	}
 
 	release_card();
 	return 0;
@@ -570,15 +615,22 @@ _nodev:
 	if (dev.device)
 		die("Don't use '-d' and '-n' options simultaneously\n");
 
-	printf("Device-less operation...\n");
-	if (!ifname || !ofname)
-		die("Have to specify both input and output files!\n");
+	printf("Device-less operation... (iwl ONLY)\n");
+
+	if (!ifname)
+		die("No input file given!\n");
+	if (patch11n && !ofname)
+		die("Have to specify output file for 802.11n patch!\n");
 	init_dump(&dev, ifname);
+
+	if (parse && dev.ops && dev.ops->eeprom_parse)
+		dev.ops->eeprom_parse(&dev);
 
 	if (patch11n && dev.ops && dev.ops->eeprom_patch11n)
 		dev.ops->eeprom_patch11n(&dev);
 
-	fixate_dump(&dev, ofname);
+	if (ofname)
+		fixate_dump(&dev, ofname);
 	return 0;
 }
 
